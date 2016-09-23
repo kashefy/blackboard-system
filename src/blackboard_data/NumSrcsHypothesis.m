ddd26da6e25bc5d1a6e8a962c8be43aace811fba
Youssef Kashef <youssef.kashef@gmail.com>
2016-09-23 18:02:57 +0200

    initial commit for no. of sources KS and Hypothesis

---
+++ src/blackboard_data/NumSrcsHypothesis.m
@@ -0,0 +1,20 @@
+classdef NumSrcsHypothesis < Hypothesis
+    
+    properties (SetAccess = private)
+        label;
+        p;                      % probability
+        n;                      % number of sources
+        concernsBlocksize_s;
+    end
+    
+    methods
+        function obj = NumSrcsHypothesis( label, p, n, blocksize_s )
+            obj = obj@Hypothesis();
+            obj.label = label;
+            obj.p = p;
+            obj.n = n;
+            obj.concernsBlocksize_s = blocksize_s;
+        end
+    end
+    
+end
